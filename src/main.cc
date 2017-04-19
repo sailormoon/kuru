96330c61edb495e872c69ad732f39412720c10b4
sailormoon <sailorswift42@gmail.com>
2017-04-18 23:32:23 -0500

    Add scope guard and main loop.

--- src/main.cc
+++ src/main.cc
@@ -1,26 +1,35 @@
 #include <SDL2/SDL.h>
 #include "glog/logging.h"
+#include "scope_guard.h"
 #include "util.h"
 
 int main() {
   using namespace spin;
-  SDL_Init(SDL_INIT_EVERYTHING);
+  CHECK_SDL(SDL_Init(SDL_INIT_EVERYTHING) == 0);
+  const scope_guard sdl_quit(SDL_Quit);
 
   const sdl_ptr<SDL_Window> window(
-      SDL_CreateWindow("spin!", 100, 100, 640, 480, SDL_WINDOW_SHOWN),
+      SDL_CreateWindow("spin!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                       640, 480, SDL_WINDOW_SHOWN),
       SDL_DestroyWindow);
-  SDL_CHECK(window);
+  CHECK_SDL(window);
 
   const sdl_ptr<SDL_Renderer> renderer(
       SDL_CreateRenderer(window.get(), -1,
                          SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC),
       SDL_DestroyRenderer);
-  SDL_CHECK(renderer);
+  CHECK_SDL(renderer);
 
-  for (int i = 0; i < 3; ++i) {
+  SDL_Event event;
+  while (true) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        return 0;
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer.get(), 128, 0, 0, 255);
     SDL_RenderClear(renderer.get());
     SDL_RenderPresent(renderer.get());
-    SDL_Delay(1000);
   }
-  SDL_Quit();
 }
---
+++ src/scope_guard.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <functional>
+
+namespace spin {
+struct scope_guard {
+  template <class Callable>
+  explicit scope_guard(Callable&& cleanup)
+      : cleanup(std::forward<Callable>(cleanup)) {}
+  ~scope_guard() { cleanup(); }
+  scope_guard(const scope_guard& other) = delete;
+  scope_guard& operator=(const scope_guard& other) = delete;
+
+ private:
+  const std::function<void()> cleanup;
+};
+}  // namespace spin
